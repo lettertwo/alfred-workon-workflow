829e7780095849eceeee75de45353ca3039e18e2
Eric Eldredge <lettertwo@gmail.com>
2013-02-12 02:31:06 -0500

    Finally, a working proof of concept

---
+++ iterm.applescript
@@ -0,0 +1,18 @@
+on run argv
+	set title to item 1 of argv
+	set dir to item 2 of argv
+	tell application "iTerm"
+		if (exists current terminal) then
+			set _term to current terminal
+		else
+			set _term to (make new terminal)
+		end if
+		tell _term
+			set _session to (launch session "Default Session")
+			tell _session
+				write text "history -d $((HISTCMD-1)); title " & title & "; cd " & dir & "; clear;"
+				-- write text "history -d $((HISTCMD-1)); cd " & dir & "; echo -e \"\\[\\033]0;" & title & "\\007\\]\"; clear;"
+			end tell
+		end tell
+	end tell
+end run
--- workon.py
+++ workon.py
@@ -2,29 +2,54 @@
 import PyAl
 import os
 import re
+import subprocess
 
-project_dir = os.path.expanduser('~/Projects')
+project_types = {
+    '.sublime-workspace': {
+        'cmd': '/usr/local/bin/subl',
+    },
+    # '.tmproj': {
+    #     'cmd': 'mate',
+    # },
+}
+
+def get_project_title(project_path):
+    return os.path.splitext(os.path.basename(project_path))[0]
+
+def serialize_project(project_path, project_type):
+    project = dict(
+        title=get_project_title(project_path),
+        subtitle=project_path,
+        arg=project_path,
+        uid=project_path,
+    )
+    project.update(project_types[project_type])
+    return project
 
 def find_projects(query='.*'):
-    query = re.compile(r'%s' % query)
-    return ((n, os.path.join(project_dir, n))
-            for n in next(os.walk(project_dir))[1] if query.search(n))
+    query = re.compile(r'%s' % query, re.IGNORECASE)
+    for t in project_types.keys():
+        type_query = re.compile(r'.+%s$' % t)
+        for p in PyAl.find(t):
+            if type_query.match(p) and query.search(p):
+                yield serialize_project(p, t)
 
 def list_projects(kw=None):
     feedback = PyAl.Feedback()
-    for project_name, project_path in find_projects(kw):
-        feedback.add(PyAl.Item(
-            title=project_name,
-            subtitle=project_path,
-            arg=project_path,
-            uid=project_path,
-            valid=True,
-        ))
+    for project in find_projects(kw):
+        meta = {'valid': True}
+        meta.update(**project)
+        feedback.add(PyAl.Item(**meta))
     return feedback
 
+def open_project(project_path):
+    title = get_project_title(project_path)
+    path = os.path.dirname(project_path)
+    if 'project' in next(os.walk(path))[1]:
+        path = os.path.join(path, 'project')
 
-def get_project(path):
-    return path
+    p = subprocess.call(['osascript', 'iterm.applescript', title, path], close_fds=True)
+
 
 if __name__ == '__main__':
-    print(list_projects('black'))
+    open_project('/Users/ede/Projects/wall.digitalrealty.loc/wall.digitalrealty.sublime-project')
