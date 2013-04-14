6fa4cf77ce47faba7efdd2b7129c878fd6eed8f3
Eric Eldredge <lettertwo@gmail.com>
2013-04-14 17:36:40 -0400

    Add alp submodule

--- workon.py
+++ workon.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
-import PyAl
+import sys
 import os
 import re
 import subprocess
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'alp'))
+import alp
+
 
 project_types = {
     '.sublime-workspace': {
@@ -13,8 +17,10 @@ project_types = {
     # },
 }
 
+
 def get_project_title(project_path):
     return os.path.splitext(os.path.basename(project_path))[0]
+
 
 def serialize_project(project_path, project_type):
     project = dict(
@@ -26,21 +32,26 @@ def serialize_project(project_path, project_type):
     project.update(project_types[project_type])
     return project
 
-def find_projects(query='.*'):
+
+def find_projects(query=None):
+    if query is None:
+        query = '.*'
     query = re.compile(r'%s' % query, re.IGNORECASE)
     for t in project_types.keys():
         type_query = re.compile(r'.+%s$' % t)
-        for p in PyAl.find(t):
+        for p in alp.find(t):
             if type_query.match(p) and query.search(p):
                 yield serialize_project(p, t)
 
+
 def list_projects(kw=None):
-    feedback = PyAl.Feedback()
+    feedback = []
     for project in find_projects(kw):
         meta = {'valid': True}
         meta.update(**project)
-        feedback.add(PyAl.Item(**meta))
-    return feedback
+        feedback.append(alp.Item(**meta))
+    return alp.feedback(feedback)
+
 
 def open_project(project_path):
     title = get_project_title(project_path)
@@ -48,8 +59,5 @@ def open_project(project_path):
     if 'project' in next(os.walk(path))[1]:
         path = os.path.join(path, 'project')
 
-    p = subprocess.call(['osascript', 'iterm.applescript', title, path], close_fds=True)
-
-
-if __name__ == '__main__':
-    open_project('/Users/ede/Projects/wall.digitalrealty.loc/wall.digitalrealty.sublime-project')
+    subprocess.call(['osascript', 'iterm.applescript', title, path],
+                    close_fds=True)
